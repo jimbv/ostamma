ed9217df03bc4e2b359189cca27a6c4301584913
José Martín <joseignaciomartin@gmail.com>
2020-10-28 15:56:58 -0300

    Update saludvirtual.blade.php

--- resources/views/web/saludvirtual.blade.php
+++ resources/views/web/saludvirtual.blade.php
@@ -14,31 +14,39 @@ Centros Médicos
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-8 col-md-10">
-                    <div class="section-title text-center pb-30">
+                    <div class="section-title text-center pb-40">
                         <h3 class="title">¿Cómo funciona AMMA Salud Virtual?</h3>
-                        <p class="text">
-                    
-                        <ul>
-                            <li>Envíe un mensaje solicitando un turno de atención.</li>
-                            <li>Indique cuáles son los síntomas por los que consulta.</li>
-                            <li>Solicitaremos estos datos: correo electrónico, teléfono de contacto, foto de DNI y de la credencial de OSTAMMA.</li>
-                            <li>Realice el pago del coseguro por Mercado Pago y envíe el comprobante.</li>
-                            <li>Se le asignará el turno.</li>
-                            <li>El día de la consulta nos contactaremos previamente por WhatsApp y le enviaremos el link de acceso al consultorio virtual. </li>
-                        </ul>
-                        <strong>Recuerde si presenta síntomas compatibles con COVID-19 debe
-                             permanecer en su domicilio y comunicarse de inmediato al 0800-888-7468
+                        
+                        
 
-                        </strong>
+                        
+                        <div class="text" style='color:white;font-size:18px;line-height:24px;text-align:left;'>
+                            <br>
+                            <ul style='list-style-type: circle;' >
+                                <li>Envíe un mensaje solicitando un turno de atención.</li>
+                                <li>Indique cuáles son los síntomas por los que consulta.</li>
+                                <li>Solicitaremos estos datos: correo electrónico, teléfono de contacto, foto de DNI y de la credencial de OSTAMMA.</li>
+                                <li>Realice el pago del coseguro por Mercado Pago y envíe el comprobante.</li>
+                                <li>Se le asignará el turno.</li>
+                                <li>El día de la consulta nos contactaremos previamente por WhatsApp y le enviaremos el link de acceso al consultorio virtual. </li>
+                            </ul>
+                            <p></p>
+                            <br> <br>
+                            <strong>
+                            <div style='font-family:OmnesBold;font-size:20px;font-weight:500;text-align:center;width:100%;'>
+                                
+                            Recuerde si presenta síntomas compatibles con COVID-19 debe
+                                permanecer en su domicilio y comunicarse de inmediato al 0800-888-7468
 
-
-
-                        .</p>
+                            </div>
+                        </div>
+ 
                     </div> <!-- section title -->
                 </div>
             </div> <!-- row -->
           
         </div> <!-- container -->
+        <br>
     </section>
 
 
