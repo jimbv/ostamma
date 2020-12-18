18d7bd8612dbeb65c03c63c1df1dabd84ce796d1
José Martín <joseignaciomartin@gmail.com>
2020-12-18 15:41:35 -0300

    Update livewire.php

--- config/livewire.php
+++ config/livewire.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'asset_url'  => '/public',
+    'asset_url'  => '/vendor/livewire/livewire.js',
 
     /*
     |--------------------------------------------------------------------------
