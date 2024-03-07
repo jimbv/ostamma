ce40cf849161a9c2ff137561fc335d6b52131f67
Jose Martin <joseignaciomartin@gmail.com>
2024-03-07 16:31:22 -0300

    Primer commit con mi código

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.template')
+@section('content')
+
+  <!-- Carrousel casos de exito -->
+  <section>
+    <div style='height:400px;padding:10px;'></div>
+
+  <img src="/imgs/slider/infinito.png" alt="slider" style="position: absolute; top:0px;left:0xp; z-index:-1;width:100%;">
+  </section>
+
+  <!-- Mozaico productos destacados -->
+  <section style="width: 100%;background-color:dark-gray;height:700px;">
+
+  </section>
+
+  <!-- Contact form -->
+  <section>
+    
+
+  </section>
+  
+
+  @if (Route::has('login'))
+  <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+    @auth
+    <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+    @else
+    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+    @if (Route::has('register'))
+    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+    @endif
+    @endauth
+  </div>
+  @endif
+
+@endsection
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Ener-tech</title>
+    <link rel="icon" type="image/x-icon" href="/imgs/logo.ico">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Enertech') }}</title>
+
+    <link href="/css/style.css?v=10" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light p-3">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/"> <img src="/imgs/enertech.png" alt="Enertech"> </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto ">
+                    <li class="nav-item">
+                        <a class="nav-link mx-2 active" aria-current="page" href="#">INICIO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-2" href="#">PRODUCTOS</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            EMPRESA
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">NOTICIAS</a></li>
+                            <li><a class="dropdown-item" href="#">GLOSARIO</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-2" href="#">CONTACTO</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto d-none d-lg-inline-flex">
+                    <li class="nav-item mx-2">
+                        Google
+                        <a target="_blank" class="redesMenu" href="https://www.instagram.com/enertech.iluminacionled/"><i class="fab fa-instagram"></i></a>
+
+                    </li>
+                    <li class="nav-item mx-2">
+
+                        <a target="_blank" class="redesMenu" href="https://www.facebook.com/enertechiluminacionled/"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a target="_blank" class="redesMenu" href="https://wa.me/5493537595618?text=Hola,%20acabo%20de%20visitar%20su%20pagina%20web%20y%20queria%20pedir%20mas%20informacion%20sobre%20sus%20productos%20y%20servicios"><i class="fab fa-whatsapp"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+    @yield('content')
+
+    <footer>
+        <div class="footer">
+            Hola estoe s una prueba
+            <img src="imgs/pie.png" alt="Enertech - Ilumninación">
+        </div>
+        <div class="copyright" style="color:white;">
+            Hola estoe s una prueba
+        </div>
+    </footer>
+
+</body>
+
+</html>
