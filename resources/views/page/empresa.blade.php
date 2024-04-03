535d3d5368add537c4f3ab5bef52d57a2e59b327
Jose Martin <joseignaciomartin@gmail.com>
2024-04-03 17:00:47 -0300

    mejora texto empresa

--- resources/views/page/empresa.blade.php
+++ resources/views/page/empresa.blade.php
@@ -24,7 +24,7 @@
                 <div class="col-md-3 pt-3">
                     <img src="/imgs/kyra_bg_empresa.jpg" alt="Fondo empresa" style="width: 100%;">
                 </div>
-                <div class="col-md-9 about-text">
+                <div class="col-md-9 about-text mb-5">
                     <div class="text-center">
                     </div> <br>
                     <p>
@@ -34,32 +34,33 @@
                     <h2 class="roboto">Innovaciones</h2>
                     <p>
                     <ul>
-                        <li><strong>DISEÑO ERGONOMICO:</strong> Nuestras luminarias no solo ofrecen iluminación eficiente, sino que también integran un diseño ergonómico que se adapta perfectamente a entornos modernos. <br></li>
-                        <li><strong>EFICIENCIA ENERGÉTICA:</strong> Incorporamos tecnologías LED de última generación para garantizar una iluminación óptima con un consumo energético mínimo. <br></li>
-                        <li><strong>PERSONALIZACIÓN:</strong> Ofrecemos soluciones personalizadas, permitiendo a nuestros clientes elegir entre una variedad de estilos, colores y configuraciones. <br></li>
+                        <li><strong>DISEÑO ERGONOMICO</strong><br>Nuestras luminarias no solo ofrecen iluminación eficiente, sino que también integran un diseño ergonómico que se adapta perfectamente a entornos modernos. <br></li>
+                        <li><strong>EFICIENCIA ENERGÉTICA</strong><br>Incorporamos tecnologías LED de última generación para garantizar una iluminación óptima con un consumo energético mínimo. <br></li>
+                        <li><strong>PERSONALIZACIÓN</strong><br>Ofrecemos soluciones personalizadas, permitiendo a nuestros clientes elegir entre una variedad de estilos, colores y configuraciones. <br></li>
                     </ul>
                     </p>
                     <p>
                     <h2>Tecnologías en la Fabricación</h2>
                     <ul>
-                        <li><strong>IMPRESIÓN 3D:</strong> Implementamos tecnologías de impresión 3D para la creación de componentes, permitiendo una producción más eficiente y flexible. <br></li>
-                        <li><strong>AUTOMATIZACIÓN ROBÓTICA:</strong> Utilizamos robots en ciertos procesos de ensamblaje, mejorando la precisión y reduciendo el tiempo de producción. <br></li>
-                        <li><strong>MATERIALES SOSTENIBLES:</strong> Adoptamos materiales respetuosos con el medio ambiente, priorizando la sostenibilidad en toda la cadena de producción. <br></li>
+                        <li><strong>IMPRESIÓN 3D</strong><br>Implementamos tecnologías de impresión 3D para la creación de componentes, permitiendo una producción más eficiente y flexible. <br></li>
+                        <li><strong>AUTOMATIZACIÓN ROBÓTICA</strong><br>Utilizamos robots en ciertos procesos de ensamblaje, mejorando la precisión y reduciendo el tiempo de producción. <br></li>
+                        <li><strong>MATERIALES SOSTENIBLES</strong><br>Adoptamos materiales respetuosos con el medio ambiente, priorizando la sostenibilidad en toda la cadena de producción. <br></li>
                     </ul>
                     </p>
 
                     <h2>Desarrollo e implementación</h2>
                     <p>
                     <ul>
-                        <li><strong>INTEGRACIÓN IOT:</strong> Nuestro productos poseen la integración de Internet de las cosas (IoT) en nuestras luminarias, permitiendo el control remoto y la personalización a través de dispositivos inteligentes. <br></li>
-                        <li><strong>TECNOLOGÍA LI-FI </strong> Investigamos el uso de tecnología Li-Fi para la transmisión de datos a través de la iluminación, abriendo nuevas posibilidades para la conectividad en entornos iluminados. <br></li>
-                        <li><strong>DISEÑO BIOFÍLICO:</strong> Nos enfocamos en el desarrollo de luminarias que incorporen elementos naturales para mejorar la conexión con la naturaleza en espacios interiores. <br></li>
+                        <li><strong>INTEGRACIÓN IOT</strong><br>Nuestro productos poseen la integración de Internet de las cosas (IoT) en nuestras luminarias, permitiendo el control remoto y la personalización a través de dispositivos inteligentes. <br></li>
+                        <li><strong>TECNOLOGÍA LI-FI</strong><br>Investigamos el uso de tecnología Li-Fi para la transmisión de datos a través de la iluminación, abriendo nuevas posibilidades para la conectividad en entornos iluminados. <br></li>
+                        <li><strong>DISEÑO BIOFÍLICO</strong><br>Nos enfocamos en el desarrollo de luminarias que incorporen elementos naturales para mejorar la conexión con la naturaleza en espacios interiores. <br></li>
                     </ul>
                     </p>
                     <p>
 
                         Nuestro compromiso con la vanguardia tecnológica y la sostenibilidad impulsa nuestro continuo desarrollo en el campo de la iluminación, garantizando soluciones que no solo iluminan, sino que también transforman espacios.
                     </p>
+                    <br>
                 </div>
             </div>
             <div class="row">
