d8ab31212435a6583f0fc1fbb06b7d530acf57f0
Jose Martin <joseignaciomartin@gmail.com>
2026-02-11 12:06:28 -0300

    test

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -44,6 +44,7 @@ class ContactController extends Controller
             'message' => $request->message,
         ];
 
+        dd(env('MAIL_TO_ADDRESS'));
         Mail::to(env('MAIL_TO_ADDRESS'))->send(new TemplateMail($content));
 
         return redirect('/contacto')->with(['success' => 'Formulario enviado correctamente']);
