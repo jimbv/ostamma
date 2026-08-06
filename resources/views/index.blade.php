da7c5321fcb45a5916c49809dddc54654bb4e9d3
Jose Martin <joseignaciomartin@gmail.com>
2026-08-06 09:23:32 -0300

    prueba

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,12 +47,12 @@
   </div>
 </div>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-<script>
+<!-- <script>
 document.addEventListener('DOMContentLoaded', function () {
     var modal = new bootstrap.Modal(document.getElementById('modalInicio'));
     modal.show();
 });
-</script>
+</script>-->
 
 <section class="position-relative w-100"
     style="height: 100vh; border-bottom: 10px solid #0097ce; overflow:hidden;">
